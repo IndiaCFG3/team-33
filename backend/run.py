b431174a33925e7282c5bebb562b0a6522423f87
Shreya Garg <shreyagarg0622@gmail.com>
2020-08-16 14:24:37 +0530

    Added Models

--- backend/run.py
+++ backend/run.py
@@ -1,1 +1,9 @@
-#RUN
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy  #for qul database
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fdd344b27cf24f67a0fc311cfd6b3573' #for qul database
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'      #for qul database
+db = SQLAlchemy(app)
