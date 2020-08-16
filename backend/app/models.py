95d7177cfcb30a4c8f4a9d2e3de9fa248012e1f1
praveshganwani <59963061+praveshganwani@users.noreply.github.com>
2020-08-16 16:01:15 +0530

    Updated Criteria

--- backend/app/models.py
+++ backend/app/models.py
@@ -31,6 +31,7 @@ class User(db.Model):
     pincode = db.Column(db.Integer(6), nullable=False)
     gender = db.Column(db.Enum(Gender), nullable=False)
     dob = db.Column(db.String(20), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
     monthly_income = db.Column(db.Numeric, nullable=False)
     education = db.Column(db.String(100), nullable=False)
     marriage_status = db.Column(db.Enum(MarriageStatus), nullable=False)
@@ -46,8 +47,13 @@ class Scheme(db.Model):
     organization = db.Column(db.String(150), nullable=False)
     private = db.Column(db.Boolean, nullable=False)
     description = db.Column(db.Text, nullable=False)
-    criteria = db.Column(db.Text, nullable=False)
-
+    criteria_city = db.Column(db.Text, nullable = True)
+    criteria_state = db.Column(db.Text, nullable=True)
+    criteria_gender = db.Column(db.Text, nullable=True)
+    criteria_monthly_income = db.Column(db.Text, nullable=True)
+    criteria_marriage_status = db.Column(db.Text, nullable=True)
+    criteria_age = db.Column(db.Text, nullable=True)
+    criteria_other = db.Column(db.Text, nullable = True)
 
 class Volunteer(db.Model):
     __tablename__ = 'volunteer'
@@ -64,6 +70,6 @@ class User_Volunteer(db.Model):
 class User_Scheme(db.Model):
     __tablename__ = 'user_scheme'
     user_id = user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True, nullable=False)
-    volunteer_id = db.Column(db.Integer, db.ForeignKey('schemes.scheme_id'), primary_key=True, nullable=False)
+    scheme_id = db.Column(db.Integer, db.ForeignKey('schemes.scheme_id'), primary_key=True, nullable=False)
     complete = db.Column(db.Boolean, nullable=False)
     status_description = db.Column(db.Text, nullable=False)
